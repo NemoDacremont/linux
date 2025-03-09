32937dc0502765b07ed58f69e7e67e5c839fa744
Catatomik <cata@catadev.org>
2025-03-09 17:49:11 +0100

    feat: add bindings helpers

--- rust/helpers/etherdevice.c
+++ rust/helpers/etherdevice.c
@@ -4,3 +4,13 @@ struct net_device *rust_helper_alloc_etherdev(int sizeof_priv)
 {
 	return alloc_etherdev(sizeof_priv);
 }
+
+void *rust_helper_netdev_priv(const struct net_device *dev)
+{
+	return netdev_priv(dev);
+}
+
+void rust_helper_eth_hw_addr_set(struct net_device *dev, const u8 *addr)
+{
+	return eth_hw_addr_set(dev, addr);
+}
